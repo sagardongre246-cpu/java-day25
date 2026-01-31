eb5a68f97407011d935ed193a710299daa8d7376
Sagar Dongare <sagardongre246@gmail.com>
2026-01-31 09:05:04 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,12 @@
+public class OddEven {
+    public static void main(String[] args) {
+
+        int number = 7; // you can change this value
+
+        if (number % 2 != 0) {
+            System.out.println("Odd Numbers");
+        } else {
+            System.out.println("Even Numbers");
+        }
+    }
+}
